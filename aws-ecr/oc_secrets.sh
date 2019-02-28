bfbdb708f7f5c560cbb37eeb94780fc01327f90f
Carolyn Thiel <cthiel@redhat.com>
2019-02-28 17:05:36 -0500

    update oc secrets script to work with OCP 3.11

--- aws-ecr/oc_secrets.sh
+++ aws-ecr/oc_secrets.sh
@@ -6,30 +6,17 @@ log() {
   echo "[$(date +%F_%H:%M:%S)] - " $@
 }
 
-SECRET_FILE=/root/secret-aws-ecr.yml
-
 # AWS ECR Login
 log "Executing AWS ECR Login"
-$(aws ecr get-login --no-include-email)
-log "New AWS ECR token generated."
 
-# Replace secret value
-dockerconfigjson_base64=`echo -n "$(cat /root/.docker/config.json)" | base64 | tr -d '\n'`
-echo -n \
-"---
-kind: Secret
-apiVersion: v1
-metadata:
-  name: aws-ecr
-data:
-  .dockerconfigjson: $dockerconfigjson_base64
-type: kubernetes.io/dockerconfigjson
-" \
-> $SECRET_FILE
+ECR_LOGIN_OUTPUT=$( aws ecr get-login --region us-east-1 --no-include-email )
+ECR_FQDN=$( echo $ECR_LOGIN_OUTPUT | cut -d' ' -f7 | cut -d'/' -f3 )
+ECR_PASSWORD=$( echo $ECR_LOGIN_OUTPUT | cut -d' ' -f6 )
+
+log "New AWS ECR token generated."
 
 log "Login as system:admin ... " && oc login -u system:admin || log "Unable to connect to Openshift cluster"
 oc version
-
 
 log "Creating/Updating secrets and configure service account on Openshift"
 
@@ -40,15 +27,14 @@ SAs=(default deployer builder)
 for project in $PROJECTS; do
   log "Project: $project"
   found=$(oc get secrets -n $project | grep aws-ecr | wc -l)
-  if [[ $found -eq 0 ]]; then
-    oc create -f /root/secret-aws-ecr.yml -n $project
-    log "Created secret aws-ecr"
-    for sa in ${SAs[@]};do
-      oc secrets add serviceaccount/$sa secrets/aws-ecr --for=pull -n $project
-      log "Added secret to serviceaccount $sa"
-    done
-  else
-    oc replace -f /root/secret-aws-ecr.yml -n $project
-    log "Updated secret aws-ecr"
+  if [[ $found -ne 0 ]]; then
+    oc delete secret aws-ecr -n $project
   fi
+  oc create secret docker-registry --docker-server=https://$ECR_FQDN --docker-username=AWS --docker-password=$ECR_PASSWORD aws-ecr -n $project
+  log "Created secret aws-ecr"
+  for sa in ${SAs[@]};do
+    oc secrets add serviceaccount/$sa secrets/aws-ecr --for=pull -n $project
+    log "Added secret to serviceaccount $sa"
+  done
 done
+
